10191c7f084ed6b880b8358cf48e084c59c3269b
kamalkumarp <kamalkumarp@outlook.com>
2017-03-16 10:58:29 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "policy-due":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    policyid = parameters.get("policy-policyid")
+
+    due = {'123':100, '234':200, '345':300, '456':400, '567':500}
+
+    speech = "Amount Due for the policy id " + policyid + " is " + str(due[policyid]) + " dollars."
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-check-policy-due"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
